5118380d3b44bfd3fc6cbe940d529d8366a8a4bd
alicecustodiodecarvalho <alice.custodio@aluno.ifsp.edu.br>
2024-09-06 17:52:58 -0300

    modal cartao

--- pages/CompraCarro.js
+++ pages/CompraCarro.js
@@ -1,8 +1,22 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import NavbarPadrao from "../components/NavbarPadrao";
-import CardCarPagamento from "../components/CardCarPagamento";
+// CompraCarro.js
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import NavbarPadrao from '../components/NavbarPadrao';
+import CardCarPagamento from '../components/CardCarPagamento';
+import CustomModal from './Modal'; // Certifique-se de que o caminho está correto
 
 export default function CompraCarro() {
+
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const openModal = () => {
+        setModalVisible(true);
+    };
+
+    const closeModal = () => {
+        setModalVisible(false);
+    };
+
     return (
         <View style={styles.bg}>
             <NavbarPadrao texto="Finalizando Compra" />
@@ -10,44 +24,45 @@ export default function CompraCarro() {
                 <Text style={styles.titulo}>Escolha a forma de pagamento</Text>
                 <CardCarPagamento />
             </View>
-                <View style={styles.pagamentos}>
-                    <TouchableOpacity style={[styles.pag, styles.pix]}><Text style={styles.texto}>Pix</Text></TouchableOpacity>
-                    <TouchableOpacity style={[styles.pag, styles.pix]}><Text style={styles.texto}>Boleto</Text></TouchableOpacity>
-                    <TouchableOpacity style={[styles.pag, styles.cart]}><Text style={styles.texto}>Cartão de Crédito/Débito</Text></TouchableOpacity>
-                </View>
+            <View style={styles.pagamentos}>
+                <TouchableOpacity style={[styles.pag, styles.pix]}><Text style={styles.texto}>Pix</Text></TouchableOpacity>
+                <TouchableOpacity style={[styles.pag, styles.pix]}><Text style={styles.texto}>Boleto</Text></TouchableOpacity>
+                <TouchableOpacity style={[styles.pag, styles.cart]} onPress={openModal}><Text style={styles.texto}>Cartão de Crédito/Débito</Text></TouchableOpacity>
+            </View>
+            <CustomModal visible={modalVisible} onClose={closeModal} />
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-    bg:{
-        backgroundColor:'#ECECEC',
-        flex:1
+    bg: {
+        backgroundColor: '#ECECEC',
+        flex: 1
     },
     container: {
         padding: 20,
         alignItems: "center"
     },
     titulo: {
-        color:'red',
-        fontWeight:'bold',
-        fontSize:17,
-        padding:5
+        color: 'red',
+        fontWeight: 'bold',
+        fontSize: 17,
+        padding: 5
     },
-    pagamentos:{
-        width:'100%',
-        backgroundColor:'white',
-        elevation:10
+    pagamentos: {
+        width: '100%',
+        backgroundColor: 'white',
+        elevation: 10
     },
-    pag:{
-        padding:10
+    pag: {
+        padding: 10
     },
-    texto:{
-        fontSize:17,
-        fontWeight:'bold'
+    texto: {
+        fontSize: 17,
+        fontWeight: 'bold'
     },
-    pix:{
-        borderColor:'lightgray',
-        borderBottomWidth:2
-    }
-})
+    pix: {
+        borderColor: 'lightgray',
+        borderBottomWidth: 2
+    },
+});
