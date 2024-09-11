1639b37fdd8dfc087f3735cb37a7e5a566550a17
alicecustodiodecarvalho <alice.custodio@aluno.ifsp.edu.br>
2024-09-11 16:05:16 -0300

    login funcionando

--- pages/Login.js
+++ pages/Login.js
@@ -1,17 +1,46 @@
-import { StyleSheet, ImageBackground, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ImageBackground, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useNavigation } from '@react-navigation/native';
 import { Image } from 'react-native';
 
 export default function Login() {
-
     const navigation = useNavigation();
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const response = await fetch('https://pi3-backend-i9l3.onrender.com/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email,
+                    senha
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                navigation.navigate('Home');
+            } else {
+                console.error('Falha ao fazer login', await response.text());
+                Alert.alert('Erro', 'Falha ao fazer login. Verifique suas credenciais.');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            Alert.alert('Erro', 'Ocorreu um erro ao tentar fazer login.');
+        }
+    };
 
     return (
         <View style={styles.container}>
             <ImageBackground
                 style={styles.bg}
-                source={require('../assets/images/background.png')}>
+                source={require('../assets/images/background.png')}
+            >
                 <View style={styles.overlay}>
                     <TouchableOpacity style={styles.voltar} onPress={() => navigation.goBack()}>
                         <MaterialIcons name="keyboard-backspace" size={24} color="black" />
@@ -21,23 +50,25 @@ export default function Login() {
                         source={require('../assets/images/logo.png')}
                     />
                     <View style={styles.forms}>
-
                         <View style={styles.inputs}>
-                        <Text style={styles.login}>Login</Text>
-
+                            <Text style={styles.login}>Login</Text>
                             <TextInput
                                 style={styles.input}
                                 placeholder="Email"
+                                value={email}
+                                onChangeText={setEmail}
+                                keyboardType='email-address'
                             />
                             <TextInput
                                 style={[styles.input, { marginBottom: 60 }]}
                                 placeholder="Senha"
-                                
+                                secureTextEntry
+                                value={senha}
+                                onChangeText={setSenha}
                             />
                         </View>
                         <View style={styles.botoes}>
-
-                            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+                            <TouchableOpacity style={styles.button} onPress={handleLogin}>
                                 <Text style={styles.text}>Entrar</Text>
                             </TouchableOpacity>
                             <Text style={styles.signupText}>
@@ -57,20 +88,19 @@ const styles = StyleSheet.create({
     },
     bg: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: 'center'
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     overlay: {
         width: '90%',
         alignItems: 'center',
-        position: 'relative'
+        position: 'relative',
     },
     logo: {
         width: 100,
         height: 100,
         resizeMode: 'contain',
         marginBottom: 190,
-
     },
     login: {
         fontSize: 26,
@@ -102,7 +132,7 @@ const styles = StyleSheet.create({
         lineHeight: 21,
         fontWeight: 'bold',
         letterSpacing: 0.25,
-        color: 'white'
+        color: 'white',
     },
     signupText: {
         color: '#333',
@@ -116,19 +146,19 @@ const styles = StyleSheet.create({
     voltar: {
         position: 'absolute',
         top: 40,
-        left: 1
+        left: 10,
     },
     forms: {
         gap: 50,
-        width: "100%",
-        alignItems: "center"
+        width: '100%',
+        alignItems: 'center',
     },
     inputs: {
-        width: "100%",
-        alignItems:'center'
+        width: '100%',
+        alignItems: 'center',
     },
-    botoes:{
-        width:"100%",
-        alignItems:"center"
-    }
-})
+    botoes: {
+        width: '100%',
+        alignItems: 'center',
+    },
+});
